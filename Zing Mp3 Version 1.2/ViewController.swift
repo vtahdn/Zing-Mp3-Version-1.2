fe11dda95809b686ced88ae9c4ee287b5cfe78e8
Viet Anh Doan <vtahdn@icloud.com>
2017-05-06 21:37:18 +0700

    cm

--- Zing Mp3 Version 1.2/ViewController.swift
+++ Zing Mp3 Version 1.2/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lb: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        img.alpha=0
+        lb.alpha=0
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 4, animations: {
+            self.img.alpha=1
+        }, completion: {
+            (finished) in UIView.animate(withDuration: 3, animations: {
+                self.lb.center=CGPoint(x: self.img.center.x, y: 100)
+                self.lb.alpha=1
+            })
+        })
+    }
+    
 }
 
